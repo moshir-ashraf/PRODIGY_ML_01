f3612fdef923f808e6e560a786a244bc15d35849
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2024-02-01 12:27:34 +0200

    Update linearRegression.py

--- linearRegression.py
+++ linearRegression.py
@@ -1,70 +1,61 @@
 import os
-
+import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 
-class houseSales:
-    def __init__(self, files):
-        self.model = LinearRegression()
-        dataFrames = []
-        for file in files:
-            dataFrames.append(pd.read_csv(os.path.join('dataset', file)))
-        self.xTrain = dataFrames[0].drop('SalePrice', axis=1)
-        self.yTrain = dataFrames[0]['SalePrice']
-        self.xTest = dataFrames[1]
-        self.yTest = dataFrames[2]['SalePrice']
-        self.preprocess()
-
-    def preprocess(self):
-        self.firstTestingId = self.xTest['Id'].iloc[0]
-        features = ['TotalSF', 'Bath', 'BedroomAbvGr', 'FullBath', 'HalfBath', 'BsmtFullBath', 'BsmtHalfBath']
-        self.xTrain['Bath'] = self.xTrain['FullBath'] + self.xTrain['BsmtFullBath'] \
-                              + (self.xTrain['HalfBath'] * 0.5) + (self.xTrain['BsmtHalfBath'] * 0.5)
-        self.xTrain['TotalSF'] = self.xTrain['TotalBsmtSF'] + self.xTrain['GrLivArea']
-        self.xTest = self.xTest.fillna(0)
-        self.xTest['Bath'] = self.xTest['FullBath'] + self.xTest['BsmtFullBath'] \
-                             + (self.xTest['HalfBath'] * 0.5) + (self.xTest['BsmtHalfBath'] * 0.5)
-        self.xTest['TotalSF'] = self.xTest['TotalBsmtSF'] + self.xTest['GrLivArea']
-        self.xTrain = self.xTrain[features]
-        self.xTest = self.xTest[features]
-
-    def train(self):
-        print('\nTraining')
-        print('--------------------------------')
-        self.xTrain.info()
-        self.model.fit(self.xTrain, self.yTrain)
-
-    def predict(self):
-        print('\nPredicting')
-        print('----------------------------')
-        self.predictions = self.model.predict(self.xTest)
-        data = {
-            'Id': range(self.firstTestingId, len(self.predictions) + self.firstTestingId),
-            'SalePrice': self.predictions
-        }
-        print(pd.DataFrame(data))
-
-    def showMetrics(self):
-        mse = mean_squared_error(self.yTest, self.predictions)
-        r2 = r2_score(self.yTest, self.predictions)
-        print(f'Mean Squared Error: {mse}')
-        print(f'R-squared: {r2}')
-
-    def plot(self):
-        plt.figure(figsize=(10, 6))
-        plt.scatter(self.yTest, self.predictions)
-        plt.xlabel("Actual Prices")
-        plt.ylabel("Predicted Prices")
-        plt.title("Actual Prices vs Predicted Prices")
-        plt.show()
+def preprocess(data):
+    data.dropna()
+    features = ['TotalBsmtSF', 'GrLivArea', 'BedroomAbvGr', 'FullBath', 'HalfBath', 'BsmtFullBath', 'BsmtHalfBath']
+    features = data[features]
+    target = data['SalePrice']
+    return train_test_split(features, target, test_size=0.3, random_state=42)
 
 
-files = ['train.csv', 'test.csv', 'sample_submission.csv']
-model = houseSales(files)
-model.train()
-model.predict()
-model.showMetrics()
-model.plot()
+def train(model, xTrain, yTrain):
+    print('\nTraining')
+    print('--------------------------------')
+    xTrain.info()
+    model.fit(xTrain, yTrain)
+    return model
+
+
+def predict(model, xTest):
+    print('\nPredicting')
+    print('----------------------------')
+    predictions = model.predict(xTest)
+    print(pd.DataFrame(predictions))
+    return model, predictions
+
+
+def showMetrics(yTest, predictions):
+    print('\n----------------------------')
+    mse = mean_squared_error(yTest, predictions)
+    r2 = r2_score(yTest, predictions)
+    evs = explained_variance_score(yTest, predictions)
+    print(f'R-squared: {r2:.6f}')
+    print(f'Mean Squared Error: {round(mse)}')
+    print(f'Explained Variance Score: {evs:.6f}')
+    print(f'Normalized Mean Squared Error (% of Variance): {mse / np.var(yTest) * 100 :.2f}%')
+    print(f'Root Mean Squared Error (% of Standard Deviation): {np.sqrt(mse) / np.std(yTest) * 100 :.2f}%')
+
+
+def showPlot(yTest, predictions):
+    plt.figure(figsize=(10, 6))
+    plt.scatter(yTest, predictions)
+    plt.xlabel("Actual Prices")
+    plt.ylabel("Predicted Prices")
+    plt.title("Actual Prices vs Predicted Prices")
+    plt.show()
+
+
+model = LinearRegression()
+data = pd.read_csv(os.path.join("dataset", "houseSales.csv"))
+xTrain, xTest, yTrain, yTest = preprocess(data)
+model = train(model, xTrain, yTrain)
+model, predictions = predict(model, xTest)
+showMetrics(yTest, predictions)
+showPlot(yTest, predictions)
